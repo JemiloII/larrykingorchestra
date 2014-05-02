5fa5abb11b9634e3d035bd88a8b7bda2b23c4475
Marcin <makepeopleup@gmail.com>
2014-05-02 17:43:20 -0500

    Destroy skrollr on mobile

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var express    = require('express'),
-    exphbr     = require('express3-handlebars'), // "express3-handlebars"
+var express = require('express'),
+    exphbr = require('express3-handlebars'), // "express3-handlebars"
     nodemailer = require('nodemailer'),
 
-    app = express(), handlebars;
+    app = express(),
+    handlebars;
 
 // Create `ExpressHandlebars` instance with a default layout.
 handlebars = exphbr.create({
     defaultLayout: 'main',
-    extname      : '.html',
+    extname: '.html',
 
     // Uses multiple partials dirs, templates in "shared/templates/" are shared
     // with the client-side of the app (see below).
@@ -23,5 +24,5 @@ app.set('view engine', 'html');
 app.use(express.bodyParser());
 require("./routes")(app, express, nodemailer);
 
-app.listen(3000);
-console.log("Larry King Orchestra server listening on: 3000, Thanks to WavaMedia!");
+app.listen(3069);
+console.log("Larry King Orchestra server listening on: 3069, Thanks to WavaMedia!");
--- assests/js/skrollr-init.js
+++ assests/js/skrollr-init.js
@@ -1,8 +1,8 @@
 //if (document.readyState == "complete" || document.readyState == "loaded") {
 // Init Skrollr
-
 //}
 
+/*
 document.onreadystatechange = function() {
     if (document.readyState === "complete") {
 
@@ -13,5 +13,19 @@ document.onreadystatechange = function() {
                 console.log(data.curTop);
             }
         })
+    }
+}
+*/
+
+document.onreadystatechange = function() {
+    // initialize skrollr if the window width is large enough
+    if (document.readyState === "complete") {
+        skrollr.init();
+    }
+
+
+    // disable skrollr if the window is resized below 768px wide
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera|android|ipad|playbook|silk Mini/i.test(navigator.userAgent)) {
+        skrollr.init().destroy(); // skrollr.init() returns the singleton created above
     }
 }
